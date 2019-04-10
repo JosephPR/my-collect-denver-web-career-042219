36947bcae07e6ffcc64172f4445512d50476babb
Joseph Reilly <reilly4181@gmail.com>
2019-04-10 17:59:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,13 @@
-d# def my_collect(array)
-#   new_arr = [] 
+ def my_collect(array)
+   new_arr = [] 
   
-#   i=0 
-#   while i < array.length 
-#   new_arr.push yield(array[i])
-#   i += 1 
-# end 
-#   # for element in array 
-#   #   new_arr.push yield element 
-#   # end 
+   i=0 
+   while i < array.length 
+   new_arr.push yield(array[i])
+   i += 1 
+ end 
+for element in array 
+ new_arr.push yield element    end 
   
-#   new_arr
-# end 
+  new_arr
+ end 
