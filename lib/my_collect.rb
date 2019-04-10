b2ca28bcf02ef4bba6ee6e30525bcb1e422e300b
Joseph Reilly <reilly4181@gmail.com>
2019-04-10 17:57:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,8 @@
 def my_collect(collections)
-    i = 0
+  
  new = []
   while i < collections.length
     yield(collections[i])
-    collections.split("")
     new.push(collections)
     i = i + 1
   end
